2ea59cd536fd57ea359fa6dacd4c64c127fac3a5
Marcus Klaas <mail@marcusklaas.nl>
2014-11-28 03:09:45 +0100

    euler35.hs

---
+++ euler35/euler35.hs
@@ -0,0 +1,18 @@
+import Data.Numbers.Primes
+
+rotations :: [a] -> [[a]]
+rotations xs = take (length xs) $ iterate rotate xs
+    where rotate xs = tail xs ++ [head xs]
+
+isCircularPrime :: String -> Bool
+isCircularPrime = (all (isPrime . read)) . rotations
+
+block :: [a] -> Int -> [[a]]
+block chars 1   = [[c] | c <- chars]
+block chars len = [c:set | c <- chars, set <- nextSet]
+    where nextSet = block chars (len - 1)
+
+inputSet :: [String]
+inputSet = concat $ map (block "1379") [1..6]
+
+main = print $ length (filter isCircularPrime inputSet) + 2
