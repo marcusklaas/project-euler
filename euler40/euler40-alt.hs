b61d5fd320073ff426c7593e526350ee70ff02cc
Marcus Klaas <mail@marcusklaas.nl>
2014-12-10 04:29:46 +0100

    make alternative solution 40 just a little bit cooler

--- euler40/euler40-alt.hs
+++ euler40/euler40-alt.hs
@@ -1,6 +1,6 @@
 import Data.Char
 
-main = print . product $ map (digitToInt . (list 1 !!) . (\n -> 10^n - 1)) [0..6]
-    where list n = show n ++ list (n + 1)
+main = print . product $ map (digitToInt . (list !!) . (10^)) [0..6]
+    where list = concatMap show [0..]
     
--- this version looks way cooler but it's actually a lot slower and less reusable than the original
+-- this version looks way cool but it's actually a lot slower and less reusable than the original
